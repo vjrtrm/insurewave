6986aaeafb1e5643be565a2b5c5537a060e7b6fd
jveerappan <jveerappan@juniper.net>
2017-07-26 17:59:46 +0530

    First Commit

---
+++ js-tools/options.js
@@ -0,0 +1,9 @@
+module.exports = {
+    development: {
+        SRC_URL: "/src/"
+    },
+
+    production: {
+        SRC_URL: "/src/"
+    }
+}
---
+++ src/js/app/home/view.js
@@ -0,0 +1,9 @@
+import BaseView from "common/view";
+import log from "core/logger";
+
+export default class extends BaseView {
+    constructor() {
+        super();
+        log.info("Init HomeView");
+    }
+}
